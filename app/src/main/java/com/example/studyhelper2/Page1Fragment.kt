0697c3484ee3671f949948deeb665f9bac91b76f
kangdongju-peng <kangdongju05@gmail.com>
2020-03-23 08:36:42 +0900

    이상해...

--- app/src/main/java/com/example/studyhelper2/Page1Fragment.kt
+++ app/src/main/java/com/example/studyhelper2/Page1Fragment.kt
@@ -1,11 +1,16 @@
 package com.example.studyhelper2
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_1.*
+import kotlinx.android.synthetic.main.fragment_1.view.*
 
 
 class Page1Fragment : Fragment() {
@@ -13,8 +18,18 @@ class Page1Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_1, container, false)
+        val root = inflater!!.inflate(R.layout.fragment_1, container, false)
+        val intent = Intent(context, EditActivity::class.java)
+        //val name = intent.getStringExtra("name").toString()
+        root.button_add.setOnClickListener {
+            Log.d("**","ho")
+            val intent = Intent(context, EditActivity::class.java)
+            startActivity(intent)
+
+        }
+        //root.textView.text = name.toString()
         return root
+
 
     }
     // 뷰 생성이 완료되면 호출되는 메소드
--- app/src/main/java/com/example/studyhelper2/Page2Fragment.kt
+++ app/src/main/java/com/example/studyhelper2/Page2Fragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_timet_p_stopw.*
+
 
 
 class Page2Fragment : Fragment(){
@@ -12,7 +14,7 @@ class Page2Fragment : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_timet_p_stopw, container, false)
+        val root = inflater.inflate(R.layout.fragment_3, container, false)
         return root
     }
     // 뷰 생성이 완료되면 호출되는 메소드
@@ -23,8 +25,8 @@ class Page2Fragment : Fragment(){
     companion object {
         private const val num = "num"
         @JvmStatic
-        fun newInstance(Number: Int): Page1Fragment {
-            return Page1Fragment().apply {
+        fun newInstance(Number: Int): Page2Fragment {
+            return Page2Fragment().apply {
                 arguments = Bundle().apply {
                     putInt(num, Number)
                 }
